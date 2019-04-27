20b234092ea52b5dcc9a6f0e87306f3c465b4a8f
benevolarX <benevolar.nouveau.project@gmail.com>
2019-04-27 20:18:38 +0200

    test

--- projets/ludum_dare_44/ludum_dare_44.js
+++ projets/ludum_dare_44/ludum_dare_44.js
@@ -12,7 +12,7 @@ async function get_dialog() {
 	let res = await fetch(LANG_URL, { method: 'GET' } );
 	return res.json();
 }
-let repliques = await get_dialog();
+let repliques = get_dialog();
 console.log(repliques.dialog);
 
 function drawMultipleLine(ctx, txt, x, y, color, font, font_size, max_w, max_x = 800)
