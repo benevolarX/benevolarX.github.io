107447c2725b94b387164be7f3c128d7a6687f4a
benevolarX <benevolar.nouveau.project@gmail.com>
2019-04-27 20:42:32 +0200

    saut ligne err

--- projets/ludum_dare_44/ludum_dare_44.js
+++ projets/ludum_dare_44/ludum_dare_44.js
@@ -48,7 +48,7 @@ function drawMultipleLine(ctx, txt, x, y, color, font, font_size, max_w, max_x =
 				line += `${mots[i]} `;
 			}
 		}
-		lines.push(mots[i - 1]);
+		lines.push(mots[i]);
 		for (i = 0; i < lines.length; i++)
 		{
 			if (i * font_size > max_x) {
