b950d91fb558af207bd7a17dbf6db4d6a184d0d8
victor-auffret <victor.auffret@orange.fr>
2025-08-09 19:15:57 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+//import sitemap from '@astrojs/sitemap';
+import solid from '@astrojs/solid-js';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://benevolarx.github.io',
+	// base: 'C:/Users/victor/Documents/perso/benevolar/dist',
+	integrations: [/*sitemap(),*/ solid()],
+	outDir: 'docs'
+});
